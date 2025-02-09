45a193a11b8396e5591ee6314988bdf3f1db6842
Zhang Guoyang <guoyang.zhang@my.jcu.edu.au>
2025-02-09 16:50:43 +0800

    Update password_stars.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,54 @@
+"""
+min_length = 8
 
-MIN_LENGTH = 8
-password = input("Please enter a password (minimum length 8): ")
-while len(password) < MIN_LENGTH:
-    print(f"Password must be at least {MIN_LENGTH} characters long.")
-    password = input("Please enter a password (minimum length 8): ")
-print('*' * len(password))
+password = input("Enter your password: ")
+while len(password) < min_length:
+    print(f"Password must be at least {min_length} characters long.")
+    password = input("Enter your password: ")
+print("*" * len(password))
+
+"""
+
+
+"""
+<Pseudocode:>
+
+function get_password(min_length)
+    get password from user
+    while length of password < min_length
+        print "Password must be at least {min_length} characters long."
+        get password from user
+    return password
+
+function print_asterisks(password)
+    print "*" repeated for length of password
+
+function main()
+    set min_length to 8
+    password = get_password(min_length)
+    print_asterisks(password)
+
+"""
+
+def get_password(min_length):
+    password = input("Enter your password: ")
+
+    while len(password) < min_length:
+        print(f"Password must be at least {min_length} characters long.")
+        password = input("Enter your password: ")
+
+    return password
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def main():
+    min_length = 8
+    password = get_password(min_length)
+    print_asterisks(password)
+
+
+if __name__ == "__main__":
+    main()
