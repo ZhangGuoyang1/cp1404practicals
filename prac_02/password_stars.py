cd86586e066112222c9953170c1126e6ad79fde8
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-09-25 16:50:38 +0800

    1

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,35 +1,3 @@
-"""
-min_length = 8
-
-password = input("Enter your password: ")
-while len(password) < min_length:
-    print(f"Password must be at least {min_length} characters long.")
-    password = input("Enter your password: ")
-print("*" * len(password))
-
-"""
-
-
-"""
-<Pseudocode:>
-
-function get_password(min_length)
-    get password from user
-    while length of password < min_length
-        print "Password must be at least {min_length} characters long."
-        get password from user
-    return password
-
-function print_asterisks(password)
-    print "*" repeated for length of password
-
-function main()
-    set min_length to 8
-    password = get_password(min_length)
-    print_asterisks(password)
-
-"""
-
 def get_password(min_length):
     password = input("Enter your password: ")
 
